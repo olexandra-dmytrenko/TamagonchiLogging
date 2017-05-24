377805eb337a092077b998f0af8ba21b25a687f5
Oleksandra_Dmytrenko <oleksandra_dmytrenko1@epam.com>
2017-05-24 12:27:34 +0300

    Added Handlers to Log4j

--- src/main/java/tamagochi/StateGenerator.java
+++ src/main/java/tamagochi/StateGenerator.java
@@ -1,29 +1,40 @@
 package tamagochi;
 
-import lombok.extern.java.Log;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Created by Oleksandra_Dmytrenko on 5/18/2017.
  * Events generator
  */
-@Log
+
 public class StateGenerator implements Runnable {
 
     private final TamagochiMind mind;
     private Thread thread;
+    Logger log = Logger.getLogger(StateGenerator.class.getName());
 
     public StateGenerator(TamagochiMind mind) {
+        setUpLogger();
         this.mind = mind;
     }
 
     public void startThread() {
         this.thread = new Thread(this);
         thread.start();
-        log.info("StateGenerator has started " + Thread.currentThread().getName());
+        log.config("StateGenerator has started " + Thread.currentThread().getName());
     }
 
+    private void setUpLogger(){
+        Handler handler = new ConsoleHandler();
+        handler.setLevel(Level.FINEST);
+        log.setLevel(Level.FINEST);
+        log.addHandler(handler);
+    }
     public void run() {
-        log.info("StateGenerator Thread has started " + Thread.currentThread().getName());
+        log.fine("StateGenerator Thread has started " + Thread.currentThread().getName());
         do {
             try {
                 mind.changeState();
@@ -31,7 +42,7 @@ public class StateGenerator implements Runnable {
                 e.printStackTrace();
             }
         } while (!mind.isTimeToDie());
-        System.out.println("State Generator Thread speaking: " + thread.getState());
+        log.finest("State Generator Thread speaking: " + thread.getState());
     }
 
 }
--- src/main/java/tamagochi/Tamagochi.java
+++ src/main/java/tamagochi/Tamagochi.java
@@ -8,7 +8,14 @@ import lombok.extern.log4j.Log4j;
 import lombok.extern.log4j.Log4j2;
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.IOException;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
+import java.util.logging.SimpleFormatter;
+import java.util.logging.XMLFormatter;
 
 import static jdk.nashorn.internal.runtime.regexp.joni.Config.log;
 
@@ -16,26 +23,31 @@ import static jdk.nashorn.internal.runtime.regexp.joni.Config.log;
  * Created by Oleksandra_Dmytrenko on 5/18/2017.
  * Observer
  */
-//@Log4j
+@Slf4j
 public class Tamagochi implements Runnable {
     private final TamagochiMind mind;
     private AtomicInteger counter = new AtomicInteger(10);
     private Thread thread;
     //The root logger resides at the top of the logger hierarchy.
     // It always exists, cannot be retrieved by name.
-    private static Logger loggerRoot = Logger.getRootLogger();
-    private static Logger logger = Logger.getLogger(Tamagochi.class.getName());
-    private static Logger loggerPack = Logger.getLogger("observerpattern");
 
     public Tamagochi(TamagochiMind mind) {
-        loggerRoot.setLevel(Level.ERROR);
-        loggerPack.setLevel(Level.DEBUG);
-        logger.setLevel(Level.INFO);
-        logger.warn("Hello warning");
-        logger.info("Hello info");
-        loggerPack.debug("Hello debug");
         this.mind = mind;
+        log.error("Slf4j hi {} ", "Sasha");
     }
+//    private void setUpLogger(){
+//        Handler handler = null;
+//        try {
+//            handler = new FileHandler("jul.log", false);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        handler.setFormatter(new SimpleFormatter());
+//        handler.setLevel(java.util.logging.Level.CONFIG);
+//        handler.publish(new LogRecord(java.util.logging.Level.WARNING, "Here I config log to test"));
+//        log.setLevel(java.util.logging.Level.FINEST);
+//        log.addHandler(handler);
+//    }
 
     public void startThread() {
         this.thread = new Thread(this);
@@ -44,7 +56,7 @@ public class Tamagochi implements Runnable {
 
     public void run() {
 
-//        log.info("Tamagochi Thread has started !!!!!!" + Thread.currentThread().getName());
+        log.info("Tamagochi Thread has started !!!!!!" + Thread.currentThread().getName());
         System.out.println("Run started with state " + mind.getState().getName());
         try {
             do {
@@ -57,7 +69,7 @@ public class Tamagochi implements Runnable {
                TamagochiState deadState = new TamagochiState(StateName.DEAD);
                mind.output(deadState);
 
-            System.out.println("Tamagochi Thread speaking: " + thread.getState());
+//            log.fine("Tamagochi Thread speaking: " + thread.getState());
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
